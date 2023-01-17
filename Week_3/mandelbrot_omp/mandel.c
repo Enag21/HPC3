8f4ec36b6f82889088b738de0c62b808166c2ee2
erik <s222930@dtu.dk>
2023-01-17 12:56:01 +0100

    Fixed bug in omp version

--- Week_3/mandelbrot_omp/mandel.c
+++ Week_3/mandelbrot_omp/mandel.c
@@ -8,25 +8,11 @@ mandel(int disp_width, int disp_height, int *array, int max_iter) {
     scale_real = 3.5 / (double)disp_width;
     scale_imag = 3.5 / (double)disp_height;
 
-	/*
+	
 	#pragma omp parallel for default(none) \
 		shared(disp_width, disp_height, scale_real, scale_imag, max_iter, array) \
 		private(i, j, x, y, u, v, u2, v2, iter) \
 		schedule(dynamic, 5) \
-		collapse(2)
-	*/
-	/*
-	#pragma omp target teams distribute parallel for default(none) \
-		shared(disp_width, disp_height, scale_real, scale_imag, max_iter, array) \
-		private(i, j, x, y, u, v, u2, v2, iter) \
-		num_teams(10) thread_limit(64) \
-		is_device_ptr(array) \
-		collapse(2)
-	*/
-
-	#pragma omp target teams distribute parallel \
-		num_teams(108) thread_limit(64) \
-		map(from: array[0:disp_height * disp_width]) \
 		collapse(2)
     for(i = 0; i < disp_width; i++) {
 		for(j = 0; j < disp_height; j++) {
