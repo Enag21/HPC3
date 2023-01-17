9974d0eca73dc2c5203195bbb32068f00459f6db
erik <s222930@dtu.dk>
2023-01-17 18:04:17 +0100

    modified for better performance

--- Week_3/matvec/gpu/mxv.cpp
+++ Week_3/matvec/gpu/mxv.cpp
@@ -13,16 +13,16 @@ double mxv_single(int m, int n, double* a, double* b, double* c){
 	double runtimeWithLoad;
 
 	double t = omp_get_wtime();
-	#pragma omp target data map(to: b[0: m * n], c[0:m]) map(from: a[0:m])
+	#pragma omp target data map(to: b[0: m * n], c[0:m]) map(from: a[0:m]) 
 	{
 		double t = omp_get_wtime();
-		#pragma omp target teams distribute parallel for \
-			num_teams(108) thread_limit(64) \
-			map(to: b[0: m * n], c[0:m]) map(from: a[0:m]) \
-			private(i, j, sum) 
+		#pragma omp target teams num_teams(108) //thread_limit(32)
+		#pragma omp distribute private(i, j, sum) 
 		for (i=0;i<m;i++){
 			sum = 0.0;
-			#pragma omp parallel for reduction(+: sum)
+			#pragma omp parallel 
+				//printf("%d\n", omp_get_num_threads());
+			#pragma omp for
 			for (j=0;j<n;j++){
 				sum += b[i*n+j]*c[j];
 			}
