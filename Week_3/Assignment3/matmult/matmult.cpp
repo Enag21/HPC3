27a6a82fb3c88a5435dce7068ffd8eca2065781a
erik <s222930@dtu.dk>
2023-01-18 10:15:45 +0100

    update

--- Week_3/Assignment3/matmult/matmult.cpp
+++ Week_3/Assignment3/matmult/matmult.cpp
@@ -1,24 +1,30 @@
 #include <stdio.h>
 
-void matmult_mkn_omp(int m,int n,int k,double *A,double *B,double *C){
+ extern void matmult_mkn_omp(int m,int n,int k,double *A,double *B,double *C){
 	if (m<=0 || n<=0 || k<=0 || A==NULL || B==NULL || C==NULL){
 		fprintf(stderr,"%s: Illegal input\n",__func__);
 		return;
 	}
-    
-    #pragma omp parallel shared(A, B, C, m, n, k)
+
+    int i, j, l, pp;
+
+    #pragma omp parallel shared(A, B, C, m, n, k) private(pp, i, l, j)
     {
         #pragma omp for
-        for (int pp=0;pp<(m*n);pp++){
-            C[pp]=0.0;
+        for (pp = 0;pp < (m * n); pp++)
+        {
+            C[pp] = 0.0;
         }
         
         #pragma omp for collapse(2) nowait
-        for (int i=0;i<m;i++){
-            for (int l=0;l<k;l++){
+        for (i = 0; i < m; i++)
+        {
+            for (l = 0; l < k; l++)
+            {
                 double a = A[i * k + l];
-                for (int j=0;j<n;j++){
-                    C[i*n+j]+= a * B[l*n+j];
+                for (j = 0; j < n; j++)
+                {
+                    C[i * n + j] += a * B[l * n + j];
                 }
             }
         }
